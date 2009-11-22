aa1f4c5712278d5c5fe21421e3a4a68153065640
Devendra Rane <ranedk@gmail.com>
2009-11-22 17:45:37 +0530

    many to many field data returns meta model info only

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ import helper
 from django.utils import simplejson
 
 class Field(object):
+    #TODO: add required field
     meta_name = 'fields'
     field_key = ''
     def __init__(self, initval=None, index=False, primary_index=False):
@@ -157,8 +158,13 @@ class ManyToManyField(Field):
             if not mtmf.val: continue
             d = []
             for f in mtmf.val: 
-                d.append(f.__data__())
-            data.update({mtmf.name : d})
+                d.append({
+                        'id' : f.id ,
+                        'modelname' : f.modelname ,
+                        'key_prefix' : f.key_prefix , 
+                })
+            f_key = "%s%s" % (mtmf.field_key, mtmf.name)
+            data.update({f_key : d})
         return data
 
     def __set__(self, obj, val):
@@ -283,8 +289,8 @@ class Model(object):
             klass.pre_save(self)
         data = self.__data__()
         key = construct_key(self.key_prefix, "id", self.id)
-        #print key, "=>", simplejson.dumps(data)
-        helper.kvds(key=key, value=simplejson.dumps(data))
+        print key, "=>", simplejson.dumps(data)
+        #helper.kvds(key=key, value=simplejson.dumps(data))
         self.is_saved = True
 
 def construct_key(key_prefix, keyname, key):
@@ -313,15 +319,23 @@ class Amodel(Model):
     a2 = Field()
     a3 = Field()
 
+class Cmodel(Model):
+    key_prefix = 'kvds__c__comment'
+    c1 = Field()
+    c2 = Field()
+    c3 = Field()
+
+
 class Comment(Model):
     key_prefix = 'kvds__comment'
     tmp = Field()
     tmp1 = ManyToManyField()
-    #tmpf = ForeignKey()
+    tmpf = ForeignKey()
 
 #sample run
 bitem = Bmodel(b1='itemb1',b2='itemb2')
 aitem = Amodel(a1='itema1', a2='itema2', a3='itema3')
-c = Comment(tmp='Rane was here', tmp1=[aitem,bitem])
+citem = Cmodel(c1='itemc1', c2='itemc2', c3='itemc3')
+c = Comment(tmp='Rane was here', tmp1=[aitem,bitem], tmpf=citem)
 
 #c.savem2m()
